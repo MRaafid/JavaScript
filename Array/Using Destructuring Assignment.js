3bf16ec0c331f76e2a9e3c07b8041f79e9419513
zero <muhammadraafid50290@gmail.com>
2024-07-29 19:33:05 +0530

    29/07/2024 @ 7:32 PM

--- Array/Using Destructuring Assignment.js
+++ Array/Using Destructuring Assignment.js
@@ -1,4 +1,20 @@
 let emp =['Zoro','23','M','Junior Developer'];
+let emp2 =['Zoro','23','M','Junior Developer'];
+let emp3 =['Zoro','23','M','Junior Developer'];
 console.group('Assess array using index number');
-    
+    console.log(emp[2]);
 console.groupEnd();
+console.group('Using Destructuring Assignment');
+    let [user, age, gender, designation] = emp
+    console.log(age);
+    // will assign values to matching index numbers
+    let [name, ,sex] = emp2
+    console.log(sex);
+    // we cn skip a element by leaving its matching index blank
+    console.group('rest')
+        let [id, Age, ...rest] = emp3
+        console.log(id); // will work similaraly to above
+        console.log(rest); 
+        // will add all the element after Age
+console.groupEnd();
+
